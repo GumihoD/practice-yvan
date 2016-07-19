9df0ccff42518ec7775c9493b0a7f5126f4261a2
yvan <yvandu@aliyun.com>
2016-07-19 16:32:05 +0800

    first commit

---
+++ src/main/java/com/yvan/practice/PracticeYvanApplication.java
@@ -0,0 +1,12 @@
+package com.yvan.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeYvanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeYvanApplication.class, args);
+	}
+}
