8181635d3e1df441af0a123d78e66f52d6847d99
yvan <yvandu@aliyun.com>
2016-07-20 15:52:32 +0800

    user model

---
+++ src/main/java/com/yvan/practice/controller/UserController.java
@@ -0,0 +1,19 @@
+package com.yvan.practice.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by yvan on 16/7/20.
+ */
+@RestController
+@RequestMapping("user")
+public class UserController {
+
+    Logger logger = LoggerFactory.getLogger(UserController.class);
+
+//    public
+
+}
---
+++ src/main/java/com/yvan/practice/utils/CommonUtils.java
@@ -0,0 +1,8 @@
+package com.yvan.practice.utils;
+
+/**
+ * Created by yvan on 16/7/20.
+ */
+public class CommonUtils {
+
+}
